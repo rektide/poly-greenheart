fcf6a3d23fc1eb40190982455d1456860b1d15ec
rektide <rektide@voodoowarez.com>
2013-07-15 20:38:51 -0400

    Greenheart respectfully agrees with doing a sensible thing upon Object.create.

--- greenheart.el
+++ greenheart.el
@@ -1,14 +1,14 @@
 <element name="x-greenheart" constructor="XGreenHeart">
 <script>
-var XGreenHeart= Object.create(HTMLElement.prototype);
+var XGreenHeartProto= Object.create(HTMLElement.prototype);
 function a404(){
 	var req= new XMLHTTPRequest()
 	req.open("GET", "//404", true)
 	req.send()
 }
-XGreenHeart.readyCallback = function() {
+XGreenHeartProto.readyCallback = function() {
 	var window.setInterval(a404, 3236)
 };
-this.register("x-greenheart",{prototype:XGreenHeart})
+var XGreenHeart= this.register("x-greenheart",{prototype:XGreenHeartProto})
 </script>
 </element>
